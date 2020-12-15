09d205272044189c7fa6320501c6a516d19d00e6
Mervin Hemaraju <mervinhemaraju16@gmail.com>
2020-12-15 18:20:04 +0400

    - Completed contact form - Completed about section - Customized Footer and copyright

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template as HTML
+from flask import Flask, render_template as HTML, request
+from flask_mail import Mail, Message
+import app.helpers.utils as Utils
 import app.portfolio as portofolio_bprint
+import os
 
 app = Flask(
     __name__,
@@ -11,7 +14,52 @@ app = Flask(
 # Register Blueprints
 app.register_blueprint(portofolio_bprint.portfolio)
 
+# SMTP Settings
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 587
+app.config["MAIL_USERNAME"] = os.environ["MAIL_USERNAME"]
+app.config["MAIL_PASSWORD"] = os.environ["MAIL_PASSWORD"]
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USE_SSL"] = False
+
+# Create a mail object
+mail = Mail(app)
+
 
 @app.errorhandler(404)
 def page_not_found(e):
     return HTML("404.html")
+
+
+# Send mail from contact form
+@app.route("/send-email", methods=["POST"])
+def send_email():
+
+    try:
+        sender_email = request.form["email"]
+
+        # Email validity check
+        if not Utils.validate_email(sender_email):
+            return (
+                "Please enter a valid email",
+                200,
+            )
+
+        msg = Message(
+            request.form["message"],
+            sender=sender_email,
+            recipients=os.environ["MAIL_RECIPIENTS"].split(","),
+        )
+        msg.body = request.form["message"]
+        msg.subject = f"{request.form['name']} :: {request.form['subject']}"
+        mail.send(msg)
+    except Exception as e:
+        return (
+            'Unfortunately, your message couldn\'t be submitted right now. Please contact me directly on <a href="mailto:th3pl4gu33@gmail.com">th3pl4gu33@gmail.com</a>.',
+            200,
+        )
+
+    # Returns a no content status code
+    # because the user doesn't need to get away
+    # from current page
+    return "OK", 200
---
+++ app/helpers/utils.py
@@ -0,0 +1,5 @@
+from validate_email import validate_email
+
+
+def is_email_valid(email):
+    return validate_email(email)
